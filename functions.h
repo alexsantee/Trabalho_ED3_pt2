620a3c3e261520b4417f17d14be50ba01011ad72
alexsantee <alexsantee@hotmail.com>
2019-12-05 20:52:56 -0300

    atualizado o header com o Dijkstra

--- functions.h
+++ functions.h
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int leregbin(FILE *fp, struct registro * reg);
 
 void limpa_reg(struct registro *reg);
@@ -8,4 +10,8 @@ void inserenografo(struct registro reg, struct grafo * grafo);
 
 void print_grafo(struct grafo grafo);
 
-void print_reg(int RRN, struct registro *reg);;
+void print_reg(int RRN, struct registro *reg);
+
+int menor_caminho(struct grafo *grafo, string cidadeOrigem,
+                    vector<int> *distancias, vector<string> *antecessores);
+
